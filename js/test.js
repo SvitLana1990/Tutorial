4ac4fd3ae7ec5a5b787fc4c2867ef538dc90c169
SvitLana1990 <svitlana112@outlook.es>
2023-07-17 21:18:53 +0200

    1

---
+++ js/test.js
@@ -0,0 +1,28 @@
+// const clients = ["Mango", "Ajax", "Poly"];
+// for (const client of clients) {
+//   console.log(client);
+// }
+
+// const string = "javascript";
+// for (const character of string) {
+//   console.log(character);
+// }
+
+// const numbers = [1, 3, 14, 18, 4, 7, 29, 6, 34];
+// const threshold = 15;
+// for (let i = 0; i < numbers.length; i += 1) {
+//   if (numbers[i] < threshold) {
+//     continue;
+//   }
+//   console.log(`Число більше за ${threshold}: ${numbers[i]}`);
+// }
+
+// Виносимо варіанти в масив
+// const redFruits = ["apple", "strawberry", "cherry", "cranberries"];
+// const fruit = "cherry";
+// // Перевіряємо присутність елемента
+// const hasFruit = redFruits.includes(fruit);
+
+// if (hasFruit) {
+//   console.log(`${fruit} is a red fruit!`);
+// }
