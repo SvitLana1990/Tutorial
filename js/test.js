09931e08d7497bbe66ee348124217b45daa1968a
SvitLana1990 <svitlana112@outlook.es>
2023-08-07 21:40:32 +0200

    4

--- js/test.js
+++ js/test.js
@@ -33,3 +33,105 @@
 
 // console.log(number < x1);
 // console.log(x1 < number || number < x2);
+// -----------------------Деструктуризація---------------------------
+// const book = {
+//   title: "The Last Kingdom",
+//   author: "Bernard Cornwell",
+//   genres: ["historical prose", "adventure"],
+//   isPublic: true,
+//   rating: 8.38,
+// };
+
+// // Деструктуризуємо
+// const { title, author, isPublic, rating, coverImage } = book;
+// console.log(book)
+// console.log(coverImage); // undefined
+
+// const accessType = isPublic ? "публічному" : "закритому";
+// const message = `Книга ${title} автора ${author} з рейтингом ${rating} знаходиться в ${accessType} доступі.`;
+// console.log(message)
+// const book = {
+//   title: "The Last Kingdom",
+//   author: "Bernard Cornwell",
+// };
+// const {
+//   title,
+//   coverImage = "https://via.placeholder.com/640/480",
+//   author,
+// } = book;
+// console.log(title);
+// console.log(author);
+// console.log(coverImage);
+// const rgb = [200, 255, 100];
+// let red, green, blue;
+
+// [red, green, blue] = rgb;
+
+// // console.log(`R:${red},G:${green},B:${blue}`);
+// const a = 10;
+// function foo(a) {
+//   a = 45;
+//   return a + a;
+// }
+// foo(a);
+// console.log(a);
+
+// function processCall(recipient, onAvailable, onNotAvailable) {
+//   // Імітуємо доступність абонента випадковим числом
+//   const isRecipientAvailable = Math.random() > 0.5;
+
+//   if (!isRecipientAvailable) {
+//     onNotAvailable(recipient);
+//     return;
+//   }
+
+//   onAvailable(recipient);
+// }
+
+// function takeCall(name) {
+//   console.log(`З'єднуємо з ${name}, очікуйте...`);
+//   // Логіка прийняття дзвінка
+// }
+
+// function activateAnsweringMachine(name) {
+//   console.log(`Абонент ${name} недоступний, залиште повідомлення.`);
+//   // Логіка активації автовідповідача
+// }
+
+// function leaveHoloMessage(name) {
+//   console.log(`Абонент ${name} недоступний, записуємо голограму.`);
+//   // Логіка запису голограми
+// }
+
+// processCall("Манго", takeCall, activateAnsweringMachine);
+// processCall("Полі", takeCall, leaveHoloMessage);
+
+// function printValue(value) {
+//   console.log(value);
+// }
+
+// function prettyPrint(value) {
+//   console.log("Logging value: ", value);
+// }
+
+// function repeat(n, action) {
+//   for (let i = 0; i < n; i += 1) {
+//     action(i);
+//   }
+// }
+
+// // Передаємо printValue як callback-функцію
+// repeat(3, printValue);
+// // 0
+// // 1
+// // 2
+
+// // Передаємо prettyPrint як callback-функцію
+// repeat(3, prettyPrint);
+
+// const numbers = [5, 10, 15, 20, 25];
+
+// const logMessage = (number, index) => {
+//   console.log(`Індекс ${index}, значення ${number}`);
+// };
+// numbers.forEach(logMessage);
